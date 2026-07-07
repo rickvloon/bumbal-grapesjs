77516bfdc6fc43a5298fc45547a93b9d796069f4
rickvl05 <rick@bumbal.eu>
2026-07-07 15:18:45 +0200

    feat: style panel buttons

--- src/panels.ts
+++ src/panels.ts
@@ -7,10 +7,16 @@ export default (editor: Editor, opts: Required<PluginOptions>) => {
   const { cmdOpenImport } = opts;
   const openExport = 'export-template';
   const openStyleManager = 'open-sm';
-  const openTraits = 'open-tm';
-  const openLayers = 'open-layers';
   const openBlocks = 'open-blocks';
   const activatePreview = 'preview';
+  const viewCommands = [openStyleManager, 'open-tm', 'open-layers', openBlocks];
+
+  // Only one view (Style Manager/Traits/Layers/Blocks) shows at a time,
+  // now that there's no button group to enforce that automatically.
+  const showView = (cmdId: string) => {
+    viewCommands.filter(id => id !== cmdId).forEach(id => editor.stopCommand(id));
+    editor.runCommand(cmdId);
+  };
   const iconStyle = 'style="display: block; max-width: 22px"';
 
   // Turn off default devices select and create new one
@@ -19,10 +25,6 @@ export default (editor: Editor, opts: Required<PluginOptions>) => {
   Panels.getPanels().reset([
     {
       id: 'commands',
-      buttons: [{}],
-    },
-    {
-      id: 'devices-c',
       buttons: [{
         id: cmdDeviceDesktop,
         command: cmdDeviceDesktop,
@@ -36,28 +38,16 @@ export default (editor: Editor, opts: Required<PluginOptions>) => {
         label: `<svg ${iconStyle} viewBox="0 0 24 24">
             <path fill="currentColor" d="M17,19H7V5H17M17,1H7C5.89,1 5,1.89 5,3V21A2,2 0 0,0 7,23H17A2,2 0 0,0 19,21V3C19,1.89 18.1,1 17,1Z" />
         </svg>`
-      }]
-    },
-    {
-      id: 'options',
-      buttons: [{
+      }, {
         id: activatePreview,
         context: activatePreview,
         command: activatePreview,
         label: `<svg ${iconStyle} viewBox="0 0 24 24"><path fill="currentColor" d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z"></path></svg>`
-      }, {
-        id: openExport,
-        command: openExport,
-        label: `<svg ${iconStyle} viewBox="0 0 24 24">
-            <path fill="currentColor" d="M12.89,3L14.85,3.4L11.11,21L9.15,20.6L12.89,3M19.59,12L16,8.41V5.58L22.42,12L16,18.41V15.58L19.59,12M1.58,12L8,5.58V8.41L4.41,12L8,15.58V18.41L1.58,12Z" />
-        </svg>`
-      }, {
-        id: cmdOpenImport,
-        command: cmdOpenImport,
-        label: `<svg ${iconStyle} viewBox="0 0 24 24">
-            <path fill="currentColor" d="M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z" />
-        </svg>`,
-      }, {
+      }],
+    },
+    {
+      id: 'middle',
+      buttons: [{
         id: 'undo',
         command: 'core:undo',
         label: `<svg ${iconStyle} viewBox="0 0 24 24">
@@ -76,52 +66,33 @@ export default (editor: Editor, opts: Required<PluginOptions>) => {
               <path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
           </svg>`,
       }],
-  }, {
-    id: 'views',
-    buttons  : [{
-      id: openStyleManager,
-      command: openStyleManager,
-      active: true,
-      label: `<svg ${iconStyle} viewBox="0 0 24 24">
-            <path fill="currentColor" d="M20.71,4.63L19.37,3.29C19,2.9 18.35,2.9 17.96,3.29L9,12.25L11.75,15L20.71,6.04C21.1,5.65 21.1,5 20.71,4.63M7,14A3,3 0 0,0 4,17C4,18.31 2.84,19 2,19C2.92,20.22 4.5,21 6,21A4,4 0 0,0 10,17A3,3 0 0,0 7,14Z" />
+    },
+    {
+      id: 'options',
+      buttons: [{
+        id: openExport,
+        command: openExport,
+        label: `<svg ${iconStyle} viewBox="0 0 24 24">
+            <path fill="currentColor" d="M12.89,3L14.85,3.4L11.11,21L9.15,20.6L12.89,3M19.59,12L16,8.41V5.58L22.42,12L16,18.41V15.58L19.59,12M1.58,12L8,5.58V8.41L4.41,12L8,15.58V18.41L1.58,12Z" />
+        </svg>`
+      }, {
+        id: cmdOpenImport,
+        command: cmdOpenImport,
+        label: `<svg ${iconStyle} viewBox="0 0 24 24">
+            <path fill="currentColor" d="M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z" />
         </svg>`,
-    }, {
-      id: openTraits,
-      command: openTraits,
-      label: `<svg ${iconStyle} viewBox="0 0 24 24">
-          <path fill="currentColor" d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z" />
-      </svg>`
-    }, {
-      id: openLayers,
-      command: openLayers,
-      label: `<svg ${iconStyle} viewBox="0 0 24 24">
-          <path fill="currentColor" d="M12,16L19.36,10.27L21,9L12,2L3,9L4.63,10.27M12,18.54L4.62,12.81L3,14.07L12,21.07L21,14.07L19.37,12.8L12,18.54Z" />
-      </svg>`
-    }, {
-      id: openBlocks,
-      command: openBlocks,
-      label: `<svg ${iconStyle} viewBox="0 0 24 24">
-          <path fill="currentColor" d="M17,13H13V17H11V13H7V11H11V7H13V11H17M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z" />
-      </svg>`
-    }],
+      }],
   }]);
 
   // On component change show the Style Manager
   opts.showStylesOnChange && editor.on('component:selected', () => {
-    const openLayersBtn = Panels.getButton('views', openLayers);
-
-    // Don't switch when the Layer Manager is on or there is no selected components
-    if((!openLayersBtn || !openLayersBtn.get('active')) && editor.getSelected()){
-      const openSmBtn = Panels.getButton('views', openStyleManager);
-      openSmBtn?.set('active', true);
-    }
+    editor.getSelected() && showView(openStyleManager);
   });
 
   // Do stuff on load
   editor.onReady(() => {
     if (opts.showBlocksOnLoad) {
-      const openBlocksBtn = Panels.getButton('views', openBlocks);
-      openBlocksBtn?.set('active', true);
+      showView(openBlocks);
     }
   });
 };
